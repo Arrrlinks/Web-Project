2e745452f66fa40f6731c88077a02867bd96c23b
Roooomain <119046014+Roooomain@users.noreply.github.com>
2023-03-15 18:26:46 +0100

    fix : Account html to php

--- account.php
+++ account.php
@@ -12,19 +12,12 @@
   <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
 </head>
 <body>
+<?php
+include 'element/burger/burger.php';
+include 'element/navbar/navbar.php';
+?>
 <img src="svgs/br-tl 1.svg" alt="tl logo" id="tl1">
 <img src="svgs/br-tl 2.svg" alt="tl logo" id="tl2">
-<div class = navbar>
-  <ion-icon name="heart-outline"></ion-icon>
-  <ion-icon name="notifications"></ion-icon>
-  <ion-icon name="person-circle-outline"></ion-icon>
-  <div class="group">
-    <svg class="icon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
-    <label>
-      <input placeholder="Recherche" type="search" class="input">
-    </label>
-  </div>
-</div>
 <div class="box-account">
   <h1 class="title">Compte utilisateur</h1>
     <div></div>
@@ -42,6 +35,7 @@
   </div>
     <img src="svgs/user-illustration.svg" alt="user illustration" id="user_illustration">
   </div>
+
 </body>
 </html>
 
