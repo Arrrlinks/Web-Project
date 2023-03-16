37d713721c6fbe2aaa603a396b35c8817c80c7b2
Arlinks <45461034+Arrrlinks@users.noreply.github.com>
2023-03-16 10:50:59 +0100

    feat: added some sidenav features

--- element/burger/burger.php
+++ element/burger/burger.php
@@ -3,12 +3,12 @@
 <div id="sidenav" class="sidenav">
     <ul>
         <li><a href="/Web-Project/index.php"><ion-icon name="home-outline"></ion-icon>Accueil</a></li>
-        <li><a href="#"><ion-icon name="briefcase-outline"></ion-icon>Navigation</a></li>
+        <li><a href="/Web-Project/search.php"><ion-icon name="briefcase-outline"></ion-icon>Navigation</a></li>
         <li><a href="#"><ion-icon name="heart-outline"></ion-icon>Wishlist</a></li>
         <li><a href="#"><ion-icon name="notifications-outline"></ion-icon>Notification</a></li>
     </ul>
     <ul class="down">
-        <li><a href="#"><ion-icon name="person-outline"></ion-icon>Mon compte</a></li>
+        <li><a href="/Web-Project/account.php"><ion-icon name="person-outline"></ion-icon>Mon compte</a></li>
         <li><a href="/Web-Project/login.php"><ion-icon name="enter-outline"></ion-icon>Connexion</a></li>
     </ul>
 </div>
