c565ffe0d0438b70d179abece8e144d34da2cf39
QuentinBessot <quentin.bessot@viacesi.fr>
2023-03-17 11:44:20 +0100

    feat : page creation , creation offre , creation utilisateur

---
+++ creationEP.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creation Utilisateur</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="creationEP.css">
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php
+include 'element/burger/burger.php';
+include 'element/navbar/navbar.php';
+?>
+<img src="svgs/br-tl 1.svg" alt="tl logo" id="tl1">
+<img src="svgs/br-tl 2.svg" alt="tl logo" id="tl2">
+<form method="post" class ="Box">
+    <h1>Creation d'utilisateur</h1>
+    <div class="Ensemble"></div>
+    <div class="ButtonR">
+    <input type="radio" name="demo5" class="demo5" id="demo5-a" checked>
+    <label for="demo5-a">Etudiant</label>
+    <input type="radio" name="demo5" class="demo5" id="demo5-b" >
+    <label for="demo5-b">Pilote</label>
+    </div>
+    <div class="NomU">
+        <label for="nomU">Nom Utilisateur </label>
+        <input type="text" id="nomU" class="test1">
+    </div>
+    <div class="PrenomU">
+        <label for="PrenomU">Prenom Utilisateur </label>
+        <input type="text" id="PrenomU" class="test1">
+    </div>
+    <div class="Centre">
+        <label for="Centre">Centre de CESI </label>
+        <input type="text" id="Centre" class="test1">
+    </div>
+    <div class="Promo">
+        <label for="Promo">Promotion d'Utilisateur </label>
+        <input type="text" id="Promo" class="test1">
+    </div>
+    <div class="buton">
+        <button type="submit" class="test">Creer </button>
+    </div>
+    </div>
+</form>
---
+++ creationOffre.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="creationOffre.css">
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php
+include 'element/burger/burger.php';
+include 'element/navbar/navbar.php';
+?>
+<img src="svgs/br-tl 1.svg" alt="tl logo" id="tl1">
+<img src="svgs/br-tl 2.svg" alt="tl logo" id="tl2">
+<form method="post" class ="Box">
+    <h1>Creation Offre</h1>
+        <div class="Nom">
+            <label for="nom">Intitulé du poste </label>
+            <input type="text" id="nom" class="test1">
+        </div>
+        <div class="Nombre">
+            <label for="nombre">Nombre de place disponible </label>
+            <input type="number" id="nombre" class="test1">
+        </div>
+        <div class="Competence">
+            <label for="competence">Conpétence  </label>
+            <input type="text" id="competence" class="test1">
+        </div>
+        <div class="Duree">
+            <label for="duree">Durée du stage  </label>
+            <input type="date" id="duree" class="test1">
+        </div>
+        <div class="Adr">
+            <label for="Adr">Adresse  </label>
+            <input type="text" id="Adr" class="test1">
+            <input type="text" id="Adr" class="test1">
+            <input type="text" id="Adr" class="test1">
+        </div>
+        <div class="Promotion">
+            <label for="Pro">Promotion  </label>
+            <input type="text" id="Pro" class="test1">
+        </div>
+        <div class="remuneration">
+            <label for="Re">Rémunération </label>
+            <input type="text" id="Re" class="test1">
+        </div>
+    <div class="buton">
+        <button type="submit" class="test">Creer </button>
+    </div>
+</form>
+</body>
+</html>
