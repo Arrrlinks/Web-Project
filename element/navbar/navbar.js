d70ed8c503fa5fccf8152f75498d798a75ddabc8
Arlinks <45461034+Arrrlinks@users.noreply.github.com>
2023-03-17 09:49:33 +0100

    fix: better responsive and many things, I don't remember what I did

--- element/navbar/navbar.js
+++ element/navbar/navbar.js
@@ -1,7 +1,7 @@
 const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 50 && window.innerWidth > 768) {
         navbar.classList.add('navbar-scroll');
     } else {
         navbar.classList.remove('navbar-scroll');
