fea14842f8c93fbaac7b8dcdfd07f29e54c9a055
Arlinks <45461034+Arrrlinks@users.noreply.github.com>
2023-03-19 19:34:01 +0100

    feat: did some changes to creation menu

---
+++ creation.js
@@ -0,0 +1,4 @@
+const adrBox = document.getElementById('adresseBox');
+function addAdr() {
+    adrBox.innerHTML += `<label for="Adr"></label><input type="text" id="Adr" class="adresse">`;
+}
--- creation.php
+++ creation.php
@@ -30,44 +30,33 @@ include 'element/burger/burger.php';
     <input type="text" id="sec" class="Cform" >
   </div>
   <div>
-    <label for="conf" >Confiance de l'Entreprise</label>
-      <div id="half-stars-example">
-          <div class="rating-group">
-              <input class="rating__input rating__input--none" checked name="rating2" id="rating2-0" value="0" type="radio">
-              <label aria-label="0 stars" class="rating__label" for="rating2-0">&nbsp;</label>
-              <label aria-label="0.5 stars" class="rating__label rating__label--half" for="rating2-05"><i class="rating__icon rating__icon--star fa fa-star-half"></i></label>
-              <input class="rating__input" name="rating2" id="rating2-05" value="0.5" type="radio">
-              <label aria-label="1 star" class="rating__label" for="rating2-10"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
-              <input class="rating__input" name="rating2" id="rating2-10" value="1" type="radio">
-              <label aria-label="1.5 stars" class="rating__label rating__label--half" for="rating2-15"><i class="rating__icon rating__icon--star fa fa-star-half"></i></label>
-              <input class="rating__input" name="rating2" id="rating2-15" value="1.5" type="radio">
-              <label aria-label="2 stars" class="rating__label" for="rating2-20"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
-              <input class="rating__input" name="rating2" id="rating2-20" value="2" type="radio">
-              <label aria-label="2.5 stars" class="rating__label rating__label--half" for="rating2-25"><i class="rating__icon rating__icon--star fa fa-star-half"></i></label>
-              <input class="rating__input" name="rating2" id="rating2-25" value="2.5" type="radio" checked>
-              <label aria-label="3 stars" class="rating__label" for="rating2-30"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
-              <input class="rating__input" name="rating2" id="rating2-30" value="3" type="radio">
-              <label aria-label="3.5 stars" class="rating__label rating__label--half" for="rating2-35"><i class="rating__icon rating__icon--star fa fa-star-half"></i></label>
-              <input class="rating__input" name="rating2" id="rating2-35" value="3.5" type="radio">
-              <label aria-label="4 stars" class="rating__label" for="rating2-40"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
-              <input class="rating__input" name="rating2" id="rating2-40" value="4" type="radio">
-              <label aria-label="4.5 stars" class="rating__label rating__label--half" for="rating2-45"><i class="rating__icon rating__icon--star fa fa-star-half"></i></label>
-              <input class="rating__input" name="rating2" id="rating2-45" value="4.5" type="radio">
-              <label aria-label="5 stars" class="rating__label" for="rating2-50"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
-              <input class="rating__input" name="rating2" id="rating2-50" value="5" type="radio">
-          </div>
-          <p class="desc" style="margin-bottom: 2rem; font-family: sans-serif; font-size:0.9rem">Half stars<br/>
-              Space on left side to select 0 stars</p>
+    <label for="conf" >Confiance de l'Entreprise</label><br>
+      <div class="rating">
+          <input type="radio" id="star1" name="rating" value="5" />
+          <label for="star1" title="1 étoile"><ion-icon name="star"></ion-icon></label>
+          <input type="radio" id="star2" name="rating" value="4" />
+          <label for="star2" title="2 étoiles"><ion-icon name="star"></ion-icon></label>
+          <input type="radio" id="star3" name="rating" value="3" />
+          <label for="star3" title="3 étoiles"><ion-icon name="star"></ion-icon></label>
+          <input type="radio" id="star4" name="rating" value="2" />
+          <label for="star4" title="4 étoiles"><ion-icon name="star"></ion-icon></label>
+          <input type="radio" id="star5" name="rating" value="1" />
+          <label for="star5" title="5 étoiles"><ion-icon name="star"></ion-icon></label>
       </div>
+
   </div>
-  <div>
+  <div id="adresseBox">
     <label>Adresse</label>
-    <label for="Adr1"></label><input type="text" id="Adr1" class="Cform">
-    <label for="Adr2"></label><input type="text" id="Adr2" class="Cform">
+      <span class="iconAdr">
+      <ion-icon name="remove-outline" ></ion-icon>
+      <ion-icon name="add-outline" onclick="addAdr()"></ion-icon>
+          </span>
+    <label for="Adr"></label><input type="text" id="Adr" class="adresse">
   </div>
   <div class="button">
-  <button type="submit" class="Cbutton">Creer </button>
+  <button type="submit" class="Cbutton">Creer</button>
   </div>
 </form>
+<script src="creation.js"></script>
 </body>
 </html>
