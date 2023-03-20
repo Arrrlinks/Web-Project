ed1ba9c2afeef807c785f202d9c5e6c19d32f30a
Arlinks <45461034+Arrrlinks@users.noreply.github.com>
2023-03-20 09:58:47 +0100

    feat: creation multiple address

--- creation.js
+++ creation.js
@@ -1,4 +1,58 @@
+let i = 0;
 const adrBox = document.getElementById('adresseBox');
 function addAdr() {
-    adrBox.innerHTML += `<label for="Adr"></label><input type="text" id="Adr" class="adresse">`;
+    i++;
+    adrBox.innerHTML += `<label for="Adr"></label><input type="text" name="Adr" id="Adr" class="adresse">`;
 }
+
+function removeAdr() {
+    if(adrBox.childElementCount > 4) {
+        adrBox.removeChild(adrBox.lastChild);
+    }
+}
+
+const nameEntreprise = document.getElementById("nameEntr");
+
+nameEntreprise.addEventListener("input", function(event) {
+    let value = event.target.value;
+    if (value.length === 0) {
+        nameEntreprise.classList.add("error");
+        event.target.setCustomValidity("Veuillez entrer un nom");
+    } else {
+        nameEntreprise.classList.remove("error");
+        event.target.setCustomValidity("");
+    }
+});
+
+const numberOfEmployee = document.getElementById("numberOfEmployee");
+
+numberOfEmployee.addEventListener("input", function(event) {
+    let value = event.target.value;
+    value = value.replace(/[^0-9]/g, "");
+    event.target.value = value;
+    if (value.length === 0) {
+        numberOfEmployee.classList.add("error");
+        event.target.setCustomValidity("Veuillez entrer un nombre");
+    } else {
+        numberOfEmployee.classList.remove("error");
+        event.target.setCustomValidity("");
+    }
+});
+
+const secteurEntreprise = document.getElementById("secteurEntr");
+
+secteurEntreprise.addEventListener("input", function(event) {
+    let value = event.target.value;
+    value = value.replace(/[^a-zA-Z\- ]/g, '');
+    event.target.value = value;
+    if (value.length === 0) {
+        secteurEntreprise.classList.add("error");
+        event.target.setCustomValidity("Veuillez entrer un secteur");
+    } else {
+        secteurEntreprise.classList.remove("error");
+        event.target.setCustomValidity("");
+    }
+});
+
+const input = document.getElementById("Adr"); // récupérer l'élément input
+const apiKey = "YOUR_API_KEY"; // remplacez YOUR_API_KEY par votre clé d'API SmartyStreets
--- creation.php
+++ creation.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Creation Entreprise</title>
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="creation.css">
-  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-  <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+    <title>Creation Entreprise</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="creation.css">
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"
+            integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
 </head>
 <body>
 <?php
@@ -16,46 +17,56 @@ include 'element/burger/burger.php';
 <img src="svgs/br-tl 1.svg" alt="tl logo" id="tl1">
 <img src="svgs/br-tl 2.svg" alt="tl logo" id="tl2">
 <form method="post" class="rectangle">
-  <h1>Creation d'une entreprise </h1><br>
-  <div>
-    <label for="name">Nom de l'entreprise</label>
-    <input type="text" id="name" class="Cform">
-  </div>
-  <div>
-    <label for="number">Nombre de personne </label>
-    <input type="text" id="number" class="Cform">
-  </div>
-  <div>
-    <label for="sec" >Secteur de l'entreprise</label>
-    <input type="text" id="sec" class="Cform" >
-  </div>
-  <div>
-    <label for="conf" >Confiance de l'Entreprise</label><br>
-      <div class="rating">
-          <input type="radio" id="star1" name="rating" value="5" />
-          <label for="star1" title="1 étoile"><ion-icon name="star"></ion-icon></label>
-          <input type="radio" id="star2" name="rating" value="4" />
-          <label for="star2" title="2 étoiles"><ion-icon name="star"></ion-icon></label>
-          <input type="radio" id="star3" name="rating" value="3" />
-          <label for="star3" title="3 étoiles"><ion-icon name="star"></ion-icon></label>
-          <input type="radio" id="star4" name="rating" value="2" />
-          <label for="star4" title="4 étoiles"><ion-icon name="star"></ion-icon></label>
-          <input type="radio" id="star5" name="rating" value="1" />
-          <label for="star5" title="5 étoiles"><ion-icon name="star"></ion-icon></label>
-      </div>
+    <h1>Creation d'une entreprise </h1><br>
+    <div>
+        <label for="nameEntr">Nom de l'entreprise</label>
+        <input type="text" name="nameEntr" id="nameEntr" class="Cform" required>
+    </div>
+    <div>
+        <label for="numberOfEmployee">Nombre de personne </label>
+        <input type="number" name="numberOfEmployee" id="numberOfEmployee" class="Cform" required>
+    </div>
+    <div>
+        <label for="secteurEntr">Secteur de l'entreprise</label>
+        <input type="text" name="secteurEntr" id="secteurEntr" class="Cform" required>
+    </div>
+    <div>
+        <label for="conf">Confiance de l'Entreprise</label><br>
+        <div class="rating">
+            <input type="radio" id="star1" name="rating" value="5" required>
+            <label for="star1" title="1 étoile">
+                <ion-icon name="star"></ion-icon>
+            </label>
+            <input type="radio" id="star2" name="rating" value="4" required>
+            <label for="star2" title="2 étoiles">
+                <ion-icon name="star"></ion-icon>
+            </label>
+            <input type="radio" id="star3" name="rating" value="3" required>
+            <label for="star3" title="3 étoiles">
+                <ion-icon name="star"></ion-icon>
+            </label>
+            <input type="radio" id="star4" name="rating" value="2" required>
+            <label for="star4" title="4 étoiles">
+                <ion-icon name="star"></ion-icon>
+            </label>
+            <input type="radio" id="star5" name="rating" value="1" required>
+            <label for="star5" title="5 étoiles">
+                <ion-icon name="star"></ion-icon>
+            </label>
+        </div>
 
-  </div>
-  <div id="adresseBox">
-    <label>Adresse</label>
-      <span class="iconAdr">
-      <ion-icon name="remove-outline" ></ion-icon>
-      <ion-icon name="add-outline" onclick="addAdr()"></ion-icon>
-          </span>
-    <label for="Adr"></label><input type="text" id="Adr" class="adresse">
-  </div>
-  <div class="button">
-  <button type="submit" class="Cbutton">Creer</button>
-  </div>
+    </div>
+    <div id="adresseBox">
+        <label>Adresse</label>
+        <span class="iconAdr">
+            <button type="button" class="addAdr" onclick="removeAdr()"><ion-icon name="remove-outline"></ion-icon></button>
+            <button type="button" class="addAdr" onclick="addAdr()"><ion-icon name="add-outline"></ion-icon></button>
+      </span>
+        <label for="Adr"></label><input type="text" name="Adr" id="Adr" class="adresse">
+    </div>
+    <div class="button">
+        <button type="submit" class="Cbutton">Creer</button>
+    </div>
 </form>
 <script src="creation.js"></script>
 </body>
