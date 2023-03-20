e86758581dc5f1ef5cad6c9b11143a2fe1425778
Arlinks <45461034+Arrrlinks@users.noreply.github.com>
2023-03-20 20:13:37 +0100

    feat: creation menu and we can now create users

--- creationUser.php
+++ creationUser.php
@@ -13,19 +13,20 @@
             integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
 </head>
 <body>
+
 <?php
 include 'element/burger/burger.php';
 include 'element/navbar/navbar.php';
 ?>
 <img src="svgs/br-tl 1.svg" alt="tl logo" id="tl1">
 <img src="svgs/br-tl 2.svg" alt="tl logo" id="tl2">
-<form method="post" class="Box">
+<form method="post" action="creationUser.php" class="Box">
     <h1>Creation d'utilisateur</h1>
     <div class="Ensemble"></div>
     <div class="ButtonR">
-        <input type="radio" name="demo5" class="demo5" id="demo5-a" checked>
+        <input type="radio" name="role" class="demo5" id="demo5-a" value="etudiant" checked required>
         <label for="demo5-a">Etudiant</label>
-        <input type="radio" name="demo5" class="demo5" id="demo5-b">
+        <input type="radio" name="role" class="demo5" id="demo5-b" value="pilote" required>
         <label for="demo5-b">Pilote</label>
     </div>
     <div class="navbarc">
@@ -40,22 +41,66 @@ include 'element/navbar/navbar.php';
         </a>
     </div>
     <div>
-        <label for="nomU">Prénom</label>
-        <input type="text" id="prenomU" class="UserCreationInput">
+        <label for="prenomU">Prénom</label>
+        <input type="text" id="prenomU" name="prenomU" class="UserCreationInput" required>
     </div>
     <div>
         <label for="nomU">Nom</label>
-        <input type="text" id="nomU" class="UserCreationInput">
+        <input type="text" id="nomU" name="nomU" class="UserCreationInput" required>
     </div>
     <div>
         <label for="Centre">Centre CESI</label>
-        <input type="text" id="Centre" class="UserCreationInput">
+        <input type="text" id="Centre" name="Centre" class="UserCreationInput" required>
     </div>
     <div>
         <label for="Promo">Promotion</label>
-        <input type="text" id="Promo" class="UserCreationInput">
+        <input type="text" id="Promo" name="Promo" class="UserCreationInput" required>
+    </div>
+    <div>
+        <label for="password">Mot de passe</label>
+        <input type="text" id="password" name="password" class="UserCreationInput" required>
     </div>
     <div class="button">
         <button type="submit" class="CreationUserButton">Creer</button>
     </div>
+    <?php
+    if (isset($_POST['prenomU']) && isset($_POST['nomU']) && isset($_POST['role']) && isset($_POST['Centre']) && isset($_POST['Promo']) && isset($_POST['password'])) {
+        $nom = $_POST['nomU'];
+        $prenom = $_POST['prenomU'];
+        $role = $_POST['role'];
+        $centre = $_POST['Centre'];
+        $promo = $_POST['Promo'];
+        $password = $_POST['password'];
+
+        $username = "cesi." . strtolower($prenom[0]) . strtolower($nom);
+        $number = 0;
+        $bdd = new PDO('mysql:host=92.222.10.61;dbname=web-project;charset=utf8', 'root', '123456789');
+
+        $req = $bdd->prepare('SELECT MAX(number) FROM users WHERE nom = :nom AND prenom = :prenom');
+        $req->execute(array(
+            'nom' => strtolower($nom),
+            'prenom' => strtolower($prenom)
+        ));
+        $resultat = $req->fetch();
+
+        if ($resultat['MAX(number)'] !== null) {
+            $username = $username . ($resultat['MAX(number)'] + 1);
+            $number = $resultat['MAX(number)'] + 1;
+        }
+
+
+        $req = $bdd->prepare('INSERT INTO users (username, nom, prenom, role, promo, centre,password,number) VALUES (:username, :nom, :prenom, :role, :promo, :centre, :password, :number)');
+        $req->execute(array(
+            'username' => $username,
+            'nom' => strtolower($nom),
+            'prenom' => strtolower($prenom),
+            'role' => strtolower($role),
+            'promo' => ucwords(strtolower($promo)),
+            'centre' => ucfirst(strtolower($centre)),
+            'password' => password_hash($password, PASSWORD_DEFAULT),
+            'number' => $number
+        ));
+        echo "L'utilisateur a bien été créé sous le nom de " . $username;
+    }
+    ?>
 </form>
