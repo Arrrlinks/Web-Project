154a5d5928baa69a7ee644d3ad4a63761e743e13
Roooomain <119046014+Roooomain@users.noreply.github.com>
2023-03-22 16:00:24 +0100

    feat: search bar in navbar and search account

--- src/controllers/navigation_c.php
+++ src/controllers/navigation_c.php
@@ -1,2 +1,6 @@
 <?php
+require_once('src/models/navigation_m.php');
+$usersResult = getUsersResult();
+$offresResult = getOffresResult();
+$entreprisesResult = getEntreprisesResult();
 require_once('view/navigation.php');
