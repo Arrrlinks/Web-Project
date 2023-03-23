44cf33cfcf72ff8cd439f3e5625652594f738762
Roooomain <119046014+Roooomain@users.noreply.github.com>
2023-03-23 10:43:10 +0100

    fix: pagination

--- src/controllers/navigation_c.php
+++ src/controllers/navigation_c.php
@@ -1,13 +1,11 @@
 <?php
 
 
-// Then include the required models and retrieve the results
 require_once('src/models/navigation_m.php');
 $page = $_GET['page'];
 $usersResult = getUsersResult();
 $offresResult = getOffresResult();
 $entreprisesResult = getEntreprisesResult();
 
-// Calculate the total number of pages based on the number of results
 $totalPages = totalPages();
 require_once('view/navigation.php');
