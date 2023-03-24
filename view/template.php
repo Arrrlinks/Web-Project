489c5d8a4c4d49a57f440fe4836dd3696de6c2e1
Roooomain <119046014+Roooomain@users.noreply.github.com>
2023-03-24 15:49:36 +0100

    feat: confirmation when the form is submit

--- view/template.php
+++ view/template.php
@@ -15,6 +15,9 @@
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"
             integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="../index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.all.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.min.css" rel="stylesheet">
+
 </head>
 <body>
 <?php
