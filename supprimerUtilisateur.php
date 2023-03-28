a61b325942e87428bde730da51b256345ee527de
Arlinks <45461034+Arrrlinks@users.noreply.github.com>
2023-03-28 10:08:49 +0200

    fix: delete user

--- supprimerUtilisateur.php
+++ supprimerUtilisateur.php
@@ -10,6 +10,10 @@ if(isAdminSession()){
 if(isset($_GET['id_utilisateur'])){
     $userdelete = $_GET['id_utilisateur'];
     $connexion = dbConnect();
+    $req = $connexion->prepare("DELETE FROM isWishlisted WHERE userId = :user");
+    $req->execute(array(':user' => $userdelete));
+    $req = $connexion->prepare("DELETE FROM hasPostulated WHERE idUser = :user");
+    $req->execute(array(':user' => $userdelete));
     $req = $connexion->prepare("DELETE FROM users WHERE id = :user");
     $req->bindParam(':user', $userdelete);
     if($req->execute()){
@@ -23,9 +27,14 @@ if(isPilotSession()){
     if(isset($_GET['id_utilisateur'])){
         $userdelete = $_GET['id_utilisateur'];
         $connexion = dbConnect();
-        $req = $connexion->prepare("DELETE FROM users WHERE id = :user and role ='etudiant'");
-        $req->bindParam(':user', $userdelete);
-        if($req->execute()){
+        $req = $connexion->prepare("DELETE FROM isWishlisted WHERE userId = :user");
+        $req->execute(array(':user' => $userdelete));
+        $req = $connexion->prepare("DELETE FROM hasPostulated WHERE idUser = :user");
+        $req->execute(array(':user' => $userdelete));
+        $req = $connexion->prepare("DELETE FROM users WHERE role ='etudiant' AND id = :user");
+        $req->execute(array(':user' => $userdelete));
+        $result = $req->fetch();
+        if($result){
             echo "success";
         } else{
             echo "error";
