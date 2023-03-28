aa9fa57c6bbe44439c60b05700fdecf51671d34f
Roooomain <119046014+Roooomain@users.noreply.github.com>
2023-03-28 19:11:36 +0200

    feat: clean searchbar when clikec

--- element/navbar/navbar.php
+++ element/navbar/navbar.php
@@ -3,7 +3,7 @@
         <svg class="searchicon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
         <label>
             <input type="hidden" name="navigation">
-            <input name="q" placeholder="Rechercher un stage" type="search" <?php if(isset($_GET['q'])){echo 'value='.$_GET['q'].'';} ?>>
+            <input name="q" id="navbar-search-input" placeholder="Rechercher un stage" type="search" <?php if(isset($_GET['q'])){echo 'value='.$_GET['q'].'';} ?>>
             <input type="hidden" name="userpage" value="1">
             <input type="hidden" name="entreprisepage" value="1">
             <input type="hidden" name="offrepage" value="1">
