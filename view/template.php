cba8bd201a766b72a2511fa3e89b5b50fb9fe58b
Roooomain <119046014+Roooomain@users.noreply.github.com>
2023-03-28 21:05:48 +0200

    feat: fav-icon

--- view/template.php
+++ view/template.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link rel="manifest" href="manifest.json">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="https://tse2.mm.bing.net/th?id=OIP.yn1B-MtM4jVkQ_O4dL4o0AHaHa&pid=Api&P=0">
+    <link rel="icon" href="img/fav-icon.png">
     <title><?= $title ?></title>
     <link rel="stylesheet" href="../styles/style.css">
     <link rel="stylesheet" href="../styles/sidenav.css">
