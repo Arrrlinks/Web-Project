bfccfb32291006bc710bfb1945bc6f0d1e1a7325
Roooomain <119046014+Roooomain@users.noreply.github.com>
2023-03-28 23:41:58 +0200

    feat: searchbar mobile

---
+++ script/navigation.js
@@ -0,0 +1,6 @@
+const input1 = document.getElementById('search-input');
+
+input1.addEventListener('click', function() {
+    input1.value = '';
+    input1.placeholder='';
+});
--- view/navigation.php
+++ view/navigation.php
@@ -8,6 +8,16 @@
 <img src="../svg/br-tl%202.svg" alt="tl logo" id="tl2">
 
 <div class="result">
+    <form action="?navigation" class="searchbar" method="get">
+        <svg class="searchicon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+        <label>
+            <input type="hidden" name="navigation">
+            <input name="q" id="search-input" placeholder="Rechercher un stage" type="search" <?php if(isset($_GET['q'])){echo 'value='.$_GET['q'].'';} ?>>
+            <input type="hidden" name="userpage" value="1">
+            <input type="hidden" name="entreprisepage" value="1">
+            <input type="hidden" name="offrepage" value="1">
+        </label>
+    </form>
 <div class="box-stage" id="box-stage">
     <h1 class="titlebox">Stages</h1>
     <?php
@@ -215,5 +225,7 @@
     <script type="text/javascript" src="supprimer.js"></script>
     <script type="text/javascript" src="supprimerOffre.js"></script>
     <script type="text/javascript" src="script/displayEntreprise.js"></script>
+    <script type="text/javascript" src="script/navigation.js"></script>
+
 <?php $content=ob_get_clean(); ?>
 <?php require('view/template.php');?>
